0a7210f5022512d368f2d495b43a149e7684db83
Tarek Chattouti <chattoutibwin@gmail.com>
2024-08-04 16:11:11 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from chatterbot import ChatBot
 from flask import Flask, render_template, request, jsonify
+import os
+import nltk
 
+# Set a custom NLTK data directory
+nltk_data_path = '/app/custom_nltk_data'
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+# Download necessary NLTK data
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.download('wordnet', download_dir=nltk_data_path)
 
 chatbot = ChatBot(
     'ExampleBot',
